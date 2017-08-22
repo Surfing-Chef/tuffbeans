3632beefbeeb552977214afaee80d0c7dd73004c
Surfing-Chef <surfing-chef@surfing-chef.com>
2017-08-22 15:17:39 -0700

    Enqueued tuff-navigation script

--- functions.php
+++ functions.php
@@ -119,6 +119,8 @@ add_action( 'widgets_init', 'tuffbeans_widgets_init' );
 function tuffbeans_scripts() {
 	wp_enqueue_style( 'tuffbeans-style', get_stylesheet_uri() );
 
+	wp_enqueue_script( 'tuffbeans-tuff-navigation', get_template_directory_uri() . '/js/tuff-navigation.js', array(), '20151215', true );
+
 	wp_enqueue_script( 'tuffbeans-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
 
 	wp_enqueue_script( 'tuffbeans-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
