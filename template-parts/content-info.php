81a281ceb20d76ea36a77529f10745225c8e98e6
Surfing-Chef <surfing-chef@surfing-chef.com>
2017-08-21 21:53:50 -0700

    UPdated front-page main nav menus style and responsive layout

--- template-parts/content-info.php
+++ template-parts/content-info.php
@@ -15,15 +15,24 @@
 		if ( is_front_page() ) : ?>
 		<section class="news-bullets">
 			<div class="news-bullet">
-				<i class="fa <?php the_field('bullet_1_icon');  ?>" aria-hidden="true"></i>
+				<span class="fa-stack fa-3x">
+				  <i class="fa fa-circle fa-stack-2x icon-background1"></i>
+				  <i class="fa <?php the_field('bullet_1_icon'); ?> fa-stack-1x"></i>
+				</span>
 				<p><?php the_field('bullet_1_info');  ?></p>
 			</div>
 			<div class="news-bullet">
-				<i class="fa <?php the_field('bullet_2_icon');  ?>" aria-hidden="true"></i>
+				<span class="fa-stack fa-3x">
+				  <i class="fa fa-circle fa-stack-2x icon-background2"></i>
+				  <i class="fa <?php the_field('bullet_2_icon'); ?> fa-stack-1x"></i>
+				</span>
 				<p><?php the_field('bullet_2_info');  ?></p>
 			</div>
 			<div class="news-bullet">
-				<i class="fa <?php the_field('bullet_3_icon');  ?>" aria-hidden="true"></i>
+				<span class="fa-stack fa-3x">
+					<i class="fa fa-circle fa-stack-2x icon-background3"></i>
+					<i class="fa <?php the_field('bullet_3_icon'); ?> fa-stack-1x"></i>
+				</span>
 				<p><?php the_field('bullet_3_info');  ?></p>
 			</div>
 		</section>
