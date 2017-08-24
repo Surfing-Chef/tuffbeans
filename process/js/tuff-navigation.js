dee8369a20ffcb5f2f91ec30ae77b996e810304d
Surfing-Chef <surfing-chef@surfing-chef.com>
2017-08-23 19:48:31 -0700

    Autogen

--- process/js/tuff-navigation.js
+++ process/js/tuff-navigation.js
@@ -1,28 +1,28 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
-  var $window = $(window);
-  var navCont = $('.main-navigation.home');
-  var navObj = $('.menu-main-nav-container');
+    var $window = $(window);
+    var navCont = $('.main-navigation.home');
+    var navObj = $('.menu-main-nav-container');
 
-  $window.scroll(function() {
-    var yPos = $window.scrollTop();
-    var check = 455;
+    $window.scroll(function() {
+        var yPos = $window.scrollTop();
+        var check = 455;
 
-    if (navObj.height() < 100 ){
-      check = check;
-    } else {
-      check = check - navObj.height()/5;
-    }
+        if (navObj.height() < 100) {
+            check = check;
+        } else {
+            check = check - navObj.height() / 5;
+        }
 
-    var htHeader = check - navCont.height();
+        var htHeader = check - navCont.height();
 
-    if (yPos >= htHeader ) {
-      navObj.addClass('top-nav-shade');
-      navCont.addClass('top-nav-docked');
-    } else {
-      navObj.removeClass('top-nav-shade');
-      navCont.removeClass('top-nav-docked');
-    }
+        if (yPos >= htHeader) {
+            navObj.addClass('top-nav-shade');
+            navCont.addClass('top-nav-docked');
+        } else {
+            navObj.removeClass('top-nav-shade');
+            navCont.removeClass('top-nav-docked');
+        }
 
     });
-  });
+});
