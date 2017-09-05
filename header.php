e094ac373cbfee25646135a5b6d01955ea7feb20
Surfing-Chef <surfing-chef@surfing-chef.com>
2017-09-05 16:25:58 -0700

    Defined calls to scripts and links in site head tag

--- header.php
+++ header.php
@@ -15,20 +15,26 @@
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="profile" href="http://gmpg.org/xfn/11">
+<!-- jQuery -->
+<script
+	src="https://code.jquery.com/jquery-3.2.1.min.js"
+	integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="
+	crossorigin="anonymous">
+</script>
+<!-- tuff-parallax -->
+<script src="<?php echo get_template_directory_uri(); ?>/js/parallax.js" ?>></script>
 
+<!-- AOS CSS -->
+<link href="https://cdn.rawgit.com/michalsnik/aos/2.1.1/dist/aos.css" rel="stylesheet">
+
+<!-- Font-Awesome -->
+<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+
+<!-- Tuff Favicon and Icon -->
 <link rel="icon" href="http://localhost/tuffPress/favicon.png" type="image/x-icon" />
 <link rel="shortcut icon" href="http://localhost/tuffPress/favicon.png" type="image/x-icon" />
 
-<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
-<link href="https://fonts.googleapis.com/css?family=Open+Sans+Condensed:700|Open+Sans:400,400i,700,700i" rel="stylesheet">
-
-<link href="https://fonts.googleapis.com/css?family=Droid+Serif:400,400i,700,700i|Oswald:400,700" rel="stylesheet">
-
-<link href="https://fonts.googleapis.com/css?family=Merriweather:400,400i,700|Montserrat:400,700" rel="stylesheet">
-
-<link href="https://fonts.googleapis.com/css?family=Raleway:400,600,700|Roboto+Slab:400,700" rel="stylesheet">
-
-<link href="https://fonts.googleapis.com/css?family=Cantarell:400,400i,700|Fjalla+One" rel="stylesheet"
+<link href="https://fonts.googleapis.com/css?family=Open+Sans:400,400i,700,700i|Montserrat:400,700" rel="stylesheet">
 
 <?php wp_head(); ?>
 </head>
